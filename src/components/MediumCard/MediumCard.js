d07d9bf3fd47e82b849865c816195204f08ecc72
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-26 13:18:14 +0200

    Added html on MediumCard component

--- src/components/MediumCard/MediumCard.js
+++ src/components/MediumCard/MediumCard.js
@@ -1,5 +1,23 @@
 const MediumCard = () => {
-    return()
-}
+  return (
+    <div className="main__aside-bottom">
+      <img
+        src="./pexels-kaique-rocha-1812602.jpg"
+        alt=""
+        className="main__big-image"
+        width="100"
+        height="100"
+      />
+      <h3 className="main__title">TITLE OF THE MAIN NEW</h3>
+      <div className="main__more">
+        <a href="h">READ MORE</a>
+        <div className="main__read-later">
+          <i>a</i>
+          <p>Add to Read Letter</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MediumCard;
