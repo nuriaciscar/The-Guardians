feeea413b9925a8232a7bca9bff8a8cd3173cf1f
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-10-26 14:03:23 +0200

    to satisfy netlify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
 import "./App.scss";
-import Header from "./components/Header/Header";
 import useNews from "./hooks/useNews";
-import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   const { news, loadNewsArticle } = useNews();
@@ -10,14 +8,7 @@ function App() {
     loadNewsArticle();
   }, [loadNewsArticle]);
 
-  //return <pre>{JSON.stringify(news, null, 2)}</pre>;
-
-  return (
-    <>
-      <Header />
-      <HomePage />
-    </>
-  );
+  return <pre>{JSON.stringify(news, null, 2)}</pre>;
 }
 
 export default App;
