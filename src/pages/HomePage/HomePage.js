e575d3165b6da246f81e4cbb11e680e301e99430
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-26 10:43:14 +0200

    Bugfixed useNews.js for sonarquebe

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,290 @@
+import styles from "Homepage.modules.css";
+
+const HomePage = () => {
+  return (
+    <div class="container">
+      <header class="header">
+        <div class="header__top-container">
+          <div class="header__logo">
+            <img src="" alt="logo the guardian" />
+          </div>
+          <div class="header__nav">
+            <div class="header__nav--user">
+              <a href="h" class="header__nav-link">
+                MY NEWS
+              </a>
+              <button class="header__nav-sign">Sign up</button>
+              <button class="header__nav-login">Login</button>
+            </div>
+            <div class="header__nav--sections">
+              <ul class="header__nav--list">
+                <li class="header__nav--list-element">NEWS</li>
+                <li class="header__nav--list-element">SPORT</li>
+                <li class="header__nav--list-element">LIFESTYLE</li>
+                <li class="header__nav--list-element">CULTURE</li>
+                <li class="header__nav--list-element">OPINION</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="header__bottom-container">
+          <div class="header__runner-list">
+            <div class="header__runner-element">
+              <p>ALOHA</p>
+            </div>
+            <div class="header__runner-element">
+              <p>ALOHA</p>
+            </div>
+            <div class="header__runner-element">
+              <p>ALOHA</p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main class="main">
+        <section class="main__news">
+          <div class="main__big">
+            <p>15.12.2021</p>
+            <img
+              src="./pexels-kaique-rocha-1812602.jpg"
+              alt=""
+              class="main__big-image"
+              width="300"
+              height="300"
+            />
+            <h3 class="main__title">TITLE OF THE MAIN NEW</h3>
+            <div class="main__more">
+              <a href="h">READ MORE</a>
+              <div class="main__read-later">
+                <i></i>
+                <p>Add to Read Letter</p>
+              </div>
+            </div>
+          </div>
+          <div class="main__aside">
+            <div class="main__aside-top">
+              <h3 class="main__title">TITLE OF THE MAIN NEW</h3>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                  <p>Add to Read Letter</p>
+                </div>
+              </div>
+            </div>
+            <div class="main__aside-bottom">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="100"
+                height="100"
+              />
+              <h3 class="main__title">TITLE OF THE MAIN NEW</h3>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                  <p>Add to Read Letter</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="section__container-title">
+            <h2 class="section__title">SPORT</h2>
+          </div>
+          <div class="section__cards">
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="section__container-title">
+            <h2 class="section__title">SPORT</h2>
+          </div>
+          <div class="section__cards">
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="section">
+          <div class="section__container-title">
+            <h2 class="section__title">SPORT</h2>
+          </div>
+          <div class="section__cards">
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+            <div class="section__card">
+              <img
+                src="./pexels-kaique-rocha-1812602.jpg"
+                alt=""
+                class="main__big-image"
+                width="200"
+                height="200"
+              />
+              <p class="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+              <div class="main__more">
+                <a href="h">READ MORE</a>
+                <div class="main__read-later">
+                  <i>a</i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer class="footer">
+        <div class="footer__top">
+          <div class="footer__info">
+            <p class="footer__title">THE CREATORS</p>
+            <p class="footer__text">aaaaaaaaaaaaaaa</p>
+            <p class="footer__text">nnnnnnnnnn</p>
+            <p class="footer__text">iiiiiiiiiiii</p>
+          </div>
+
+          <div class="footer__info">
+            <p class="footer__title">THE CREATORS</p>
+            <p class="footer__text">aaaaaaaaaaaaaaa</p>
+            <p class="footer__text">nnnnnnnnnn</p>
+            <p class="footer__text">iiiiiiiiiiii</p>
+          </div>
+
+          <div class="footer__info">
+            <p class="footer__title">THE CREATORS</p>
+            <p class="footer__text">aaaaaaaaaaaaaaa</p>
+            <p class="footer__text">nnnnnnnnnn</p>
+            <p class="footer__text">iiiiiiiiiiii</p>
+          </div>
+        </div>
+
+        <div class="footer__copyright">CopyRight tralalalalallalalalal</div>
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
