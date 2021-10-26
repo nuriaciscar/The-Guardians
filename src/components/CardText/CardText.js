975016c2300a2f12923b47eeeb49190585a94995
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-26 13:15:01 +0200

    Added html on CardText component

---
+++ src/components/CardText/CardText.js
@@ -0,0 +1,16 @@
+const CardText = () => {
+  return (
+    <div className="main__aside-top">
+      <h3 className="main__title">TITLE OF THE MAIN NEW</h3>
+      <div className="main__more">
+        <a href="h">READ MORE</a>
+        <div className="main__read-later">
+          <i>a</i>
+          <p>Add to Read Letter</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardText;
