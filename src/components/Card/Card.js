1c9db26ab8ad6fbd6678d5c0bad0b58073da0ba3
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-26 15:54:28 +0200

    Tested successfully component Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,23 @@
-const Card = () => {
+const Card = ({ image, text, info, icon }) => {
   return (
-    <div className="section__card">
+    <div className="section__card" data-testid="square">
       <img
-        src="./pexels-kaique-rocha-1812602.jpg"
+        src={image}
         alt=""
         className={"main__big-image"}
         width="200"
         height="200"
       />
-      <p className="section__text">Text adfskjladfsjkjadfskjladfsl</p>
+      <p className="section__text">{text} </p>
       <div className="main__more">
-        <a href="h">READ MORE</a>
+        <a href="h">{info}</a>
         <div className="main__read-later">
-          <i>a</i>
+          <i>{icon}</i>
         </div>
       </div>
     </div>
   );
 };
 
+// content.fields.thumbnail
 export default Card;
