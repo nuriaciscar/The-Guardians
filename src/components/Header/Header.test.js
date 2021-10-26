5bdc88e88b550eb63f85c92fad9616e197156149
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-10-26 16:09:39 +0200

    added Header test and changed test script to include verbose and coverage

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Given a Header component,", () => {
+  describe("It recives nothing, so...", () => {
+    test("Then it renders a header", () => {
+      render(<Header />);
+    });
+  });
+});
