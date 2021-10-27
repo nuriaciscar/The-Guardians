9f668a8f4629682e55ae4a1b955847a21f0f040a
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-27 16:05:25 +0200

    Added env file

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,7 +6,16 @@ import useNews from "../../hooks/useNews";
 import { useEffect, useState } from "react";
 
 const HomePage = () => {
-  const { news, loadNewsList, newsSport, loadNewsListSport, newsScience, loadNewsListScience, newsLifeStyle, loadNewsListLifeStyle } = useNews();
+  const {
+    news,
+    loadNewsList,
+    newsSport,
+    loadNewsListSport,
+    newsScience,
+    loadNewsListScience,
+    newsLifeStyle,
+    loadNewsListLifeStyle,
+  } = useNews();
   const [numberSport, setNumberSport] = useState(1);
   const [numberScience, setNumberScience] = useState(1);
   const [numberLifeStyle, setNumberLifeStyle] = useState(1);
@@ -15,7 +24,15 @@ const HomePage = () => {
     loadNewsListSport("sport", numberSport);
     loadNewsListScience("science", numberScience);
     loadNewsListLifeStyle("lifeandstyle", numberLifeStyle);
-  }, [loadNewsList, loadNewsListSport, loadNewsListScience, loadNewsListLifeStyle, numberSport, numberScience, numberLifeStyle]);
+  }, [
+    loadNewsList,
+    loadNewsListSport,
+    loadNewsListScience,
+    loadNewsListLifeStyle,
+    numberSport,
+    numberScience,
+    numberLifeStyle,
+  ]);
 
   let date = "";
   let image = "";
@@ -31,27 +48,24 @@ const HomePage = () => {
   const reloadSport = (event) => {
     if (event.target.outerText === "+") {
       setNumberSport(numberSport + 3);
-    }
-    else if (numberSport > 1) {
+    } else if (numberSport > 1) {
       setNumberSport(numberSport - 3);
     }
-  }
+  };
   const reloadScience = (event) => {
     if (event.target.outerText === "+") {
       setNumberScience(numberScience + 3);
-    }
-    else if (numberScience > 1) {
+    } else if (numberScience > 1) {
       setNumberScience(numberScience - 3);
     }
-  }
+  };
   const reloadLifeStyle = (event) => {
     if (event.target.outerText === "+") {
       setNumberLifeStyle(numberLifeStyle + 3);
-    }
-    else if (numberLifeStyle > 1) {
+    } else if (numberLifeStyle > 1) {
       setNumberLifeStyle(numberLifeStyle - 3);
     }
-  }
+  };
 
   if (news.response !== undefined) {
     const newsList = news.response.results;
@@ -62,20 +76,23 @@ const HomePage = () => {
     id = dateClean[0].id;
     titleCartText = newsList[1].webTitle;
     idText = newsList[1].id;
-    titleCartMedium = newsList[2].webTitle
+    titleCartMedium = newsList[2].webTitle;
     imageCartMedium = newsList[2].fields.thumbnail;
     idMedium = newsList[2].id;
-
   }
   return (
     <>
-      {newsSport.length !== 0 && news.response !== undefined &&
+      {newsSport.length !== 0 && news.response !== undefined && (
         <main className="main">
           <section className="main__news">
             <MainCard date={date} image={image} title={title} key={id} />
             <div className="main__aside">
               <CardText text={titleCartText} key={idText} />
-              <MediumCard image={imageCartMedium} text={titleCartMedium} key={idMedium} />
+              <MediumCard
+                image={imageCartMedium}
+                text={titleCartMedium}
+                key={idMedium}
+              />
             </div>
           </section>
           <section className="section">
@@ -86,7 +103,12 @@ const HomePage = () => {
             <div className="section__cards">
               <button onClick={reloadSport}>-</button>
               {newsSport.map((newCardSport) => (
-                <Card text={newCardSport.webTitle} image={newCardSport.fields.thumbnail} key={newCardSport.id} />))}
+                <Card
+                  text={newCardSport.webTitle}
+                  image={newCardSport.fields.thumbnail}
+                  key={newCardSport.id}
+                />
+              ))}
               <button onClick={reloadSport}>+</button>
             </div>
           </section>
@@ -98,7 +120,12 @@ const HomePage = () => {
             <div className="section__cards">
               <button onClick={reloadScience}>-</button>
               {newsScience.map((newCardScience) => (
-                <Card text={newCardScience.webTitle} image={newCardScience.fields.thumbnail} key={newCardScience.id} />))}
+                <Card
+                  text={newCardScience.webTitle}
+                  image={newCardScience.fields.thumbnail}
+                  key={newCardScience.id}
+                />
+              ))}
               <button onClick={reloadScience}>+</button>
             </div>
           </section>
@@ -110,11 +137,17 @@ const HomePage = () => {
             <div className="section__cards">
               <button onClick={reloadLifeStyle}>-</button>
               {newsLifeStyle.map((newCardLifeStyle, i) => (
-                <Card text={newCardLifeStyle.webTitle} image={newCardLifeStyle.fields.thumbnail} key={newCardLifeStyle.id} />))}
+                <Card
+                  text={newCardLifeStyle.webTitle}
+                  image={newCardLifeStyle.fields.thumbnail}
+                  key={newCardLifeStyle.id}
+                />
+              ))}
               <button onClick={reloadLifeStyle}>+</button>
             </div>
           </section>
-        </main>}
+        </main>
+      )}
     </>
   );
 };
