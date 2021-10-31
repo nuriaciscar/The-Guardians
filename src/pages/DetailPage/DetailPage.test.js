8b1c91378af6208cfec3d215ac3cb6c0e3a61a88
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-10-31 20:48:25 +0100

    added two tests to DetailPage

--- src/pages/DetailPage/DetailPage.test.js
+++ src/pages/DetailPage/DetailPage.test.js
@@ -1,6 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import DetailPage from "./DetailPage";
-import getNewsDetails from "../../factories/detailFactory";
 import NewsContextProvider from "../../store/contexts/NewsContextProvider";
 import { MemoryRouter } from "react-router-dom";
 import { serverArticleApiGuardian } from "../../mocks/servers";
@@ -10,15 +9,11 @@ describe("Given a DetailPage component,", () => {
     serverArticleApiGuardian.listen();
   });
 
-  afterEach(() => {
-    serverArticleApiGuardian.resetHandlers();
-  });
-
   afterAll(() => {
     serverArticleApiGuardian.close();
   });
 
-  describe("When is invoked", () => {
+  describe("When it's rendered", () => {
     test("Then it renders the DetailPage", () => {
       render(
         <NewsContextProvider>
@@ -33,35 +28,66 @@ describe("Given a DetailPage component,", () => {
       );
     });
 
-    // test("Then it renders the detail page with the content it has received", () => {
-    //   render(
-    //     <DetailPage
-    //       sectionName={details.sectionName}
-    //       articleDate={details.articleDate}
-    //       imageSource={details.imageSource}
-    //       articleTitle={details.articleTitle}
-    //       articleSubtitle={details.articleSubtitle}
-    //       bodyText={details.bodyText}
-    //     />
-    //   );
+    test("Then it renders the detail page with the content it has received", async () => {
+      render(
+        <NewsContextProvider>
+          <MemoryRouter
+            initialEntries={[
+              "/details/?id=football/live/2021/oct/23/crystal-palace-v-newcastle-leeds-v-wolves-and-more-football-clockwatch-live",
+            ]}
+          >
+            <DetailPage />
+          </MemoryRouter>
+        </NewsContextProvider>
+      );
 
-    //   const sectionRendered = screen.getByText(details.sectionName);
-    //   const dateRendered = screen.getByText(details.articleDate);
+      const sectionRendered = await screen.findByRole("heading", {
+        name: "football",
+      });
+      const dateRendered = await screen.findByText("2021-10-23");
 
-    //   const titleRendered = screen.getByText(details.articleTitle);
-    //   const subtitleRendered = screen.getByText(details.articleSubtitle);
-    //   const textRendered = screen.getByText(details.bodyText);
+      const titleRendered = await screen.findByText(
+        "Everton 2-5 Watford, Crystal Palace 1-1 Newcastle and more: football clockwatch – as it happened"
+      );
+      const subtitleRendered = await screen.findByText(
+        "Watford secured a huge win at Everton, mainly thanks to a Josh King hat-trick"
+      );
+      const textRendered = await screen.findByText(
+        "King becoming a goalscorer once more is great news for the squad as you need plenty of goals to stay up. Maxwel Cornet is good. The late Rodrigo penalty ensures the points are shared. All square at Selhurst Park after Benteke’s second was ruled out."
+      );
 
-    //   const imageRendered = screen.getByAltText(
-    //     "thumbnail of the article. Api not descriptive enough, sorry"
-    //   );
+      const imageRendered = await screen.findByAltText(
+        "thumbnail of the article. Api not descriptive enough, sorry"
+      );
 
-    //   expect(sectionRendered).toBeInTheDocument();
-    //   expect(dateRendered).toBeInTheDocument();
-    //   expect(imageRendered.src).toBe(details.imageSource);
-    //   expect(titleRendered).toBeInTheDocument();
-    //   expect(subtitleRendered).toBeInTheDocument();
-    //   expect(textRendered).toBeInTheDocument();
-    // });
+      await waitFor(() => {
+        expect(sectionRendered).toBeInTheDocument();
+        expect(dateRendered).toBeInTheDocument();
+        expect(imageRendered.src).toBe(
+          "https://media.guim.co.uk/abed4aada136d1ec38ce0e2b109c77683153d6b6/0_119_2583_1551/500.jpg"
+        );
+        expect(titleRendered).toBeInTheDocument();
+        expect(subtitleRendered).toBeInTheDocument();
+        expect(textRendered).toBeInTheDocument();
+      });
+    });
+
+    test("Then it calls the Api one time", async () => {
+      render(
+        <NewsContextProvider>
+          <MemoryRouter
+            initialEntries={[
+              "/details/?id=football/live/2021/oct/23/crystal-palace-v-newcastle-leeds-v-wolves-and-more-football-clockwatch-live",
+            ]}
+          >
+            <DetailPage />
+          </MemoryRouter>
+        </NewsContextProvider>
+      );
+
+      await waitFor(() =>
+        expect(serverArticleApiGuardian).toHaveBeenCalledTimes(1)
+      );
+    });
   });
 });
